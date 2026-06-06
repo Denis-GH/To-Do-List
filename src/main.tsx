7e1dd4f4efb01c12d461b0c96d7dcb825b3507e0
Denis Vakulenko <00vakulenko@gmail.com>
2026-06-06 14:54:20 +0500

    feat(styles): add common, normalize and palette styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './views/App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './views/App';
+
+import './views/styles/normalize.scss';
+import './views/styles/palette.scss';
+import './views/styles/common.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
